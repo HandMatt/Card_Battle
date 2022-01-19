139179516323e09bb50f2728eb8deda9f6c83302
HandMatt <74497505+HandMatt@users.noreply.github.com>
2022-01-19 18:47:51 +0000

    First commit

---
+++ js/scenes.js
@@ -0,0 +1,49 @@
+;(function(){
+  var game = this.cardBattleGame = this.cardBattleGame || {};
+
+  // Generic Scene object.
+  var scene = game.scene = {
+    node: document.querySelector('.scene'),
+    setup: function(){},
+    onShow: function(){}, // hook for child objects to use.
+    show: function() {
+      this.node.classList.remove('out');
+      this.node.classList.add('in');
+      this.onShow();
+    },
+    hide: function() {
+      this.node.classList.remove('in');
+      this.node.classList.add('out');
+    }
+  };
+
+  // Start Scene
+  var startScene = game.startScene = Object.create(scene);
+  startScene.node = document.getElementById('start-scene');
+  startScene.setup = function() {
+    document.getElementById('start-btn').onclick = function() {
+      game.flow.startGame();
+      return false;
+    };
+  };
+
+  // Gameover Scene
+  var gameOverScene = game.gameOverScene = Object.create(scene);
+  gameOverScene.node = document.getElementById('gameover-scene');
+  gameOverScene.setup = function() {
+    document.getElementById('back-to-menu-button').onclick = function() {
+      game.flow.startOver();
+    };
+  };
+  gameOverScene.onShow = function() {
+    if (game.HP.isPlayerDead()) {
+      this.node.classList.add('lose');
+      this.node.classList.remove('win');
+    } else {
+      this.node.classList.add('win');
+      this.node.classList.remove('lose');
+    }
+  }
+  
+})();
+
